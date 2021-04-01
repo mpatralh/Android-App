4d3a219073c1a75ec4073d75c930ed238300f745
MariaBolka <mariabolka98@gmail.com>
2021-04-01 22:42:52 +0300

    to merge

---
+++ app/src/main/java/com/example/sportsapp/configAthlima.java
@@ -0,0 +1,15 @@
+package com.example.sportsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class configAthlima extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config_athlima);
+
+    }
+}
